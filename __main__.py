17ad6ceaddea3aa10d3c9d2f39dd9c0103cf63ce
antic <santic16@raf.rs>
2019-04-05 23:45:09 +0200

    Modified prompt message

--- __main__.py
+++ __main__.py
@@ -1,19 +1,23 @@
 from interpreters import Interpreter
 from lexer import Lexer
 
+INFIX, PREFIX, POSTFIX = ('INFIX', 'PREFIX', 'POSTFIX')
+
 
 def main():
+    state = INFIX
     while True:
-        try:
-            text = input('INFIX'+' --> ')
-        except EOFError:
-            break
+        text = input(state+' --> ')
 
         if not text:
             continue
 
-        if text == 'exit':
+        if text.lower() == 'exit' or text.lower() == 'exit()':
             break
+
+        if text.upper() in (INFIX, PREFIX, POSTFIX):
+            state = text.upper()
+            continue
 
         lexer = Lexer(text)
         interpreter = Interpreter(lexer)
